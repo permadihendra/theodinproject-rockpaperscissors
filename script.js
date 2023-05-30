fa36e801cd083286d8fa94ee6b802ff891daefba
permadihendra <26792223+permadihendra@users.noreply.github.com>
2023-05-30 10:22:39 +0700

    Add comment to code explanation

--- script.js
+++ script.js
@@ -3,24 +3,26 @@
 // if playerSelection == computerSelection
 //     then output "Draw"
 
+// Function to get computer choice/ input
 function getComputerChoice(choice) {
   choice = "Rock";
   return choice;
 }
 
+// Function to get player choice/input with promt windows
 function getPlayerChoice(choice) {
   choice = prompt("Choose Rock, Paper, or Scissor ?");
   return choice;
 }
 
-// let playerSelection = prompt("Choose Rock, Paper, or Scissor ?");
-// const computerSelection = getComputerChoice();
+// Initial variable to save points
 let playerPoints = 0;
 let computerPoints = 0;
 
+// Function to compute conditional of rock paper scissor, with 2 parameter input
 function playRound(playerSelection, computerSelection) {
   let results;
-  //   return `You Lose! ${playerSelection} beats ${computerSelection}`;
+
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
     results = "You Draw!";
   } else if (
@@ -55,15 +57,18 @@ function playRound(playerSelection, computerSelection) {
     playerPoints = playerPoints + 1;
   }
 
+  // Function return the PLayer and Computer Point
   return `Player Point: ${playerPoints} vs Computer Points: ${computerPoints}`;
 }
 
+// Function game to loop the game
 function game() {
   const round = 5;
   for (let i = 0; i < round; i++) {
     console.log(playRound(getPlayerChoice(), getComputerChoice()));
   }
 
+  // this conditional to check who is the winner
   if (playerPoints < computerPoints) {
     console.log("Player Lose!");
   } else if (playerPoints > computerPoints) {
@@ -71,4 +76,5 @@ function game() {
   } else console.log("Draw");
 }
 
+// Start the game / call game() function
 game();
